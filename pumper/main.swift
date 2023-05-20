a3d4c36a0af7fa84ca16c3e59b4c7123e48a406a
Bill Donner <billdonner@gmail.com>
2023-05-20 16:56:07 -0500

    safety

--- pumper/main.swift
+++ pumper/main.swift
@@ -4,6 +4,8 @@
 //  Created by bill donner on 5/7/23.
 //
 
+// this runs on ios for experimentation only, its a mac program really
+
 import Foundation
 
 
@@ -15,11 +17,15 @@ struct ChatGPTChoice: Codable {
   let text: String
 }
 
-func callChapGPT(prompt:String,
+let apiURL = "https://api.openai.com/v1/completions"
+
+func callChapGPT(tag:String,
+                 nodots:Bool,
+                 verbose:Bool,
+                 prompt:String,
                  outputting: @escaping (String)->Void ,wait:Bool = false ) throws {
   
   let apiKey = "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"
-  let apiURL = "https://api.openai.com/v1/completions"
   
   guard let url = URL(string: apiURL) else {
     fatalError("Invalid API URL")
@@ -29,8 +35,9 @@ func callChapGPT(prompt:String,
   request.httpMethod = "POST"
   request.setValue("application/json", forHTTPHeaderField: "Content-Type")
   request.setValue("Bearer " + apiKey, forHTTPHeaderField: "Authorization")
+  request.timeoutInterval = 120
   
-   var respo:String = ""
+  var respo:String = ""
   
   let parameters: [String: Any] = [
     "prompt": prompt,
@@ -42,35 +49,35 @@ func callChapGPT(prompt:String,
     "temperature": 1.0
   ]
   request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
-  print("Prompt: \(prompt) \n\n>awaiting response...",terminator:"")
+  if verbose {
+    print("\n>Prompt #\(tag): \n\(prompt) \n\n>Awaiting response #\(tag) from chatGPT.",terminator:"")
+  }
   let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
     guard let data = data, error == nil else {
       print(error?.localizedDescription ?? "Unknown error")
       return
     }
     do {
-       let response = try JSONDecoder().decode(ChatGPTResponse.self, from: data)
-       respo  = response.choices.first?.text ?? "<<nothin>>"
-      
-      //let respo = String(decoding:data,as:UTF8.self)
-       outputting(respo)
+      let response = try JSONDecoder().decode(ChatGPTResponse.self, from: data)
+      respo  = response.choices.first?.text ?? "<<nothin>>"
+      outputting(respo)
     }  catch {
-       print ("Failed to decode response ",error,respo)
+      print ("Failed to decode response ",error,respo)
     }
   }
   task.resume()
   // linger here if asked to wait
   if wait {
     while true   {
-      sleep(1)
-      print(".",terminator: "")
+      if !nodots {print(".",terminator: "")}
       if respo != "" { break }
+      sleep(10)
     }
   }
 }
 #if os(iOS)
 
-struct SplitFile {
+struct Pumper {
   
   var url: String = "https://billdonner.com/fs/gd/pumper-data.txt"
   
@@ -78,15 +85,23 @@ struct SplitFile {
   
   var comments_pattern = "///"
   
+  var `repeat` =  1
+  
+  var nodots = true
+  
+  var verbose = false
+  
+  var dontcall = false
+  
 }
 #else
 
 import ArgumentParser
 
-struct SplitFile: ParsableCommand {
+struct Pumper: ParsableCommand {
   
   static let configuration = CommandConfiguration(
-    abstract: "Split up a file of ChatGPT prompts and send them to ChatGPT.",
+    abstract: "Split up a file of Prompts and pump them in to the AI Assistant",
     version: "0.1.1",
     subcommands: [],
     defaultSubcommand: nil,
@@ -102,11 +117,72 @@ struct SplitFile: ParsableCommand {
   @Argument(help: "The pattern to use to indicate a comments line")
   var comments_pattern: String
   
-}
+  @Option(name:.long, help:"Output file for AI JSON utterances")
+  var output: String
   
+  @Option(name: .shortAndLong, help: "Read input N times, creating more prompts")
+  var `repeat`: Int = 1
+  
+  @Option(name: .shortAndLong, help: "Don't print dots whilst waiting")
+  var nodots: Bool = false
+  
+  @Option(name: .shortAndLong, help: "Print a lot more")
+  var verbose: Bool = true
+  
+  @Option(name: .shortAndLong, help: "Don't call AI")
+  var dontcall: Bool = false
+  
+}
+
 #endif
-extension SplitFile {
+extension Pumper {
   func run() throws {
+    
+  
+    var tagval:Int = 0
+    var fileHandle:FileHandle? = nil
+    defer {
+      if let fileHandle = fileHandle {
+       try! fileHandle.close()
+      }
+    }
+
+    func extractSubstringsInBrackets(input: String) -> [String] {
+      var matches: [String] = []
+      var completed: [Bool] = []
+      var idx = 0 // into matches
+      var inside = false
+      matches.append("")
+      completed.append(false)
+      for c in input {
+        switch c {
+        case "{":
+          if inside { continue }//fatalError("already inside")}
+          inside = true
+          matches [idx].append("{")
+          completed [idx] =  false
+        case "}":
+          if !inside {  continue }//fatalError("not inside")}
+          inside = false
+          matches [idx].append("}")
+          completed [idx] = true
+          idx += 1
+          matches.append("")
+          completed.append(false)
+          
+        default: if inside {
+          matches [idx].append(c)
+        }
+        }
+      }
+      return matches.filter {
+        $0 != ""
+      }
+    }
+    func cleanup(string:String) -> String {
+      let jsons = extractSubstringsInBrackets(input: string)
+      return jsons.joined(separator: ",")
+    }
     func stripComments(source: String, commentStart: String) -> String {
       let lines = source.split(separator: "\n")
       var keeplines:[String] = []
@@ -118,17 +194,47 @@ extension SplitFile {
       return keeplines.joined(separator: "\n")
     }
     if let url = URL(string:url) {
-      // Get the contents of the file.
-      let contents = try String(contentsOf: url)
-      print(">URL: \(contents.count) bytes")
-      // Split the contents of the file into chunks using the pattern.
-      let chunks = contents.split(separator: split_pattern)
-      for chunk in chunks {
-        let prompt = stripComments(source: String(chunk), commentStart: comments_pattern)
-        if prompt.count > 0 {
-          try callChapGPT(prompt : prompt,outputting:  { response in
-            print("response:\(response)")
-          }, wait:true)
+      if output != "" {
+        guard let outurl = URL(string:output) else {
+          print("Bad output url") ; return
+        }
+       fileHandle = try FileHandle(forWritingTo: outurl)
+      }
+      for idx in 1...`repeat` {
+        // Get the contents of the file.
+        let contents = try String(contentsOf: url)
+        if idx == 1 && verbose {
+          print(">Prompts url: \(url)  (\(contents.count) bytes)")
+          print(">Contacting: \(apiURL)\n\n")
+        }
+        // Split the contents of the file into chunks using the pattern.
+        let chunks = contents.split(separator: split_pattern)
+        for chunk in chunks {
+          let prompt = stripComments(source: String(chunk), commentStart: comments_pattern)
+          if prompt.count > 0 {
+            tagval += 1
+            let tag = String(format:"%03d",tagval) +  " \(Date())"
+            if dontcall {
+              print("\n>Deliberately not calling AI for prompt #\(tag):")
+              print(prompt)
+              sleep(3)
+            } else {
+              try callChapGPT(tag:tag, nodots: nodots,
+                              verbose:verbose ,prompt : prompt,
+                              outputting:  { response in
+                let cleaned = cleanup(string: response)
+                
+                if verbose {
+                  print("\n>AI Response #\(tag):")
+                }
+                print("\n\(cleaned)")
+                if let fileHandle = fileHandle  {
+                  // append response
+                  fileHandle.write(cleaned.data(using: .utf8)!)
+                }
+              }, wait:true)
+            }
+          }
         }
       }
     }
@@ -139,7 +245,7 @@ extension SplitFile {
   }
 }
 #if os(iOS)
-try SplitFile().run()
+try Pumper().run()
 #else
-SplitFile.main()
+Pumper.main()
 #endif
