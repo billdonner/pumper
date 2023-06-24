1d7eb124d867b9204072683a072a8963f9c522a1
bill donner <billdonner@gmail.com>
2023-06-24 08:32:41 -0500

    moving parts of pumper upstream

--- pumper/callAI.swift
+++ pumper/callAI.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 import q20kshare
+
 let ChatGPTModel = "text-davinci-003"
+
 fileprivate func callChapGPT(pumper:Pumper, outputURL:URL,
                  tag:String,
                  prompt:String,
@@ -106,42 +108,8 @@ func callTheAI(pumper:Pumper, _ outputURL:URL,_ tag: String, _ prompt: String, _
     print("\n>AI Response #\(tag): ***ERROR \(error) no challenges returned in \(elapsed) secs\n")
   }
 }
-//write swift function to return substring of a string between a start delimeter and end delimeter.
-//include comments
 
-/*
- This function takes a string and two delimiters as arguments.
- It will return the substring which begins after the first delimiter and ends before the second one.
-   -Parameter str: The original string which is to be searched
-   -Parameter startDelim: The substring that is beginning of the substring to be extracted
-   -Parameter endDelim: The substring that is the end of the substring to be extracted
-   -Returns: A substring beginning after startDelim and ending before endDelim
-*/
-func extractSubstring(str: String, startDelim: String, endDelim: String) -> String {
-    if !str.contains(startDelim) || !str.contains(endDelim) { return "" }
-    
-    let start = str.firstIndex(of: startDelim.first!)!
-    let end = str.firstIndex(of: endDelim.first!)!
 
-    return String(str[start...end])
-}
-fileprivate func getOpinion(_ xitem:String) throws -> Opinion? {
-  let item = extractSubstring(str: xitem, startDelim: "{", endDelim: "}")
-  guard item != "" else { print("** nothing found in getOpinion from \(xitem)"); return nil }
-  var opinion:Opinion? = nil
-  do {
-    opinion = try JSONDecoder().decode(AIOpinion .self,from:item.data(using:.utf8)!).toOpinion(source: ChatGPTModel)
-  }
-  catch {
-    do {
-      opinion = try JSONDecoder().decode(AIAltOpinion .self,from:item.data(using:.utf8)!).toOpinion(source: ChatGPTModel)
-    }
-    catch {
-      print("*** No opinion found \(error)\n item: '\(item)'")
-    }
-  }
-  return opinion
-}
  
 fileprivate func handleVeracityMode(_ item:String) throws {
   let opinion = try getOpinion(item)
